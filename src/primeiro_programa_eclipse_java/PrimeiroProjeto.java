279d7b6c1045b5b78c8abd57e7aee00f9ba6d510
Mirela <Mirela@DESKTOP-OHILUQ7>
2021-09-26 13:27:21 -0300

    first commit in this project

---
+++ src/primeiro_programa_eclipse_java/PrimeiroProjeto.java
@@ -0,0 +1,16 @@
+package primeiro_programa_eclipse_java;
+
+import primeiro_programa_eclipse_java.pack2.Gato;
+
+public class PrimeiroProjeto {
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("Bichilin", "Cinza", 3);
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello, World!"+ " " + (a+b));*/
+		
+	}
+}
