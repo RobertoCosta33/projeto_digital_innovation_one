9d3175ae1cc14b7ddec74447265d324f01b053c5
Roberto B. Costa <romeromatallica@gmail.com>
2021-09-26 14:16:20 -0300

    Modifiquei os tipos das variaveis e metodos para double

--- src/primeiro_programa_eclipse_java/calculadora/Calculadora.java
+++ src/primeiro_programa_eclipse_java/calculadora/Calculadora.java
@@ -1,23 +1,45 @@
 package primeiro_programa_eclipse_java.calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double a = 0;
+		double b = 0;
 		
+		System.out.print("Digite o primeiro número: ");
+		a = scan.nextInt();
+		
+		System.out.print("Digite o segundo número: ");
+		b = scan.nextInt();
+		
+		double soma = soma(a, b);
+		double subtracao = subtracao(a, b);
+		double multiplicacao = multiplicacao(a, b);
+		double divisao = divisao(a, b);
+		
+		System.out.println("soma " + soma);
+		System.out.println("subtracao " + subtracao);
+		System.out.println("multiplicacao " + multiplicacao);
+		System.out.println("divisao " + divisao);
 	}
 	
-	public static int soma(int a, int b) {
+	public static double soma(double a, double b) {
 		return a + b;
 	}
 	
-	public static int subtracao(int a, int b) {
+	public static double subtracao(double a, double b) {
 		return a - b;
 	}
 	
-	public static int multiplicacao(int a, int b) {
+	public static double multiplicacao(double a, double b) {
 		return a * b;
 	}
 	
-	public static int divisao(int a, int b) {
+	public static double divisao(double a, double b) {
 		return a / b;
 	}
+	
+	
 }
