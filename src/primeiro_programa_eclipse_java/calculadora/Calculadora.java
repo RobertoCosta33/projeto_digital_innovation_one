fe58bfbd461e40b8110fe5df7c4afd2ed5b1680b
Roberto B. Costa <romeromatallica@gmail.com>
2021-09-26 13:51:08 -0300

    Criei quatro metodos, um para cada operação básica matemática.

--- src/primeiro_programa_eclipse_java/calculadora/Calculadora.java
+++ src/primeiro_programa_eclipse_java/calculadora/Calculadora.java
@@ -1,5 +1,23 @@
 package primeiro_programa_eclipse_java.calculadora;
 
 public class Calculadora {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+	
+	public static int divisao(int a, int b) {
+		return a / b;
+	}
 }
